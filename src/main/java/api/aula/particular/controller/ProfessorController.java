5c5b8224bcc80a07e96831022aca70e20b4cbd3a
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2023-07-25 21:45:12 -0300

    Trabalhando com migrations e criando tabela

--- src/main/java/api/aula/particular/controller/ProfessorController.java
+++ src/main/java/api/aula/particular/controller/ProfessorController.java
@@ -4,6 +4,7 @@ import api.aula.particular.professor.DadosCadastroProfessor;
 import api.aula.particular.professor.Professor;
 import api.aula.particular.professor.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class ProfessorController {
     private ProfessorRepository repository;
 
     @PostMapping //Quando chegar uma requisição do tipo Post, ira chamar este metodo
+    @Transactional // Anotação para transações 
     public void cadastrar(@RequestBody DadosCadastroProfessor dados){ //Anotação para receber o corpo da requisição
        repository.save(new Professor(dados));
     }
---
+++ src/main/resources/db/migration/V1__create-table-professores.sql
@@ -0,0 +1,16 @@
+CREATE TABLE professores (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    cpf VARCHAR(6) NOT NULL UNIQUE,
+    disciplina VARCHAR(100) NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    complemento VARCHAR(100),
+    numero VARCHAR(20),
+    uf CHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL
+);
+
+
